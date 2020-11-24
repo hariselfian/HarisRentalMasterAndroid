c15e30723bc46212770fce028bf43542974e2733
hariselfian <harisdj1996@gmail.com>
2020-11-25 00:40:42 +0700

    Buat Navigation View

--- app/src/main/java/com/hariselfian/harisrental/MainActivity.kt
+++ app/src/main/java/com/hariselfian/harisrental/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hariselfian.harisrental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.hariselfian.harisrental.fragment.AkunFragment
+import com.hariselfian.harisrental.fragment.HomeFragment
+import com.hariselfian.harisrental.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome: Fragment = HomeFragment()
+    val fragmentAkun: Fragment = AkunFragment()
+    val fragmentKeranjang: Fragment = KeranjangFragment()
+    val fm: FragmentManager = supportFragmentManager
+    val active: Fragment = fragmentHome
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
     }
 }
