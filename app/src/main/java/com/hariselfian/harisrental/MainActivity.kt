91861ab12bd71163ab66bb3f8302bd16709af5f7
hariselfian <harisdj1996@gmail.com>
2020-11-25 01:56:52 +0700

    Done Navigation View

--- app/src/main/java/com/hariselfian/harisrental/MainActivity.kt
+++ app/src/main/java/com/hariselfian/harisrental/MainActivity.kt
@@ -2,26 +2,65 @@ package com.hariselfian.harisrental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hariselfian.harisrental.fragment.AkunFragment
 import com.hariselfian.harisrental.fragment.HomeFragment
 import com.hariselfian.harisrental.fragment.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
 
-    val fragmentHome: Fragment = HomeFragment()
-    val fragmentAkun: Fragment = AkunFragment()
-    val fragmentKeranjang: Fragment = KeranjangFragment()
-    val fm: FragmentManager = supportFragmentManager
-    val active: Fragment = fragmentHome
+    private val fragmentHome: Fragment = HomeFragment()
+    private val fragmentKeranjang: Fragment = KeranjangFragment()
+    private var fragmentAkun: Fragment = AkunFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active: Fragment = fragmentHome
+
+    private lateinit var menu: Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpBottomNav()
+    }
+    fun setUpBottomNav(){
         fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
-        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
         fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+        menu =bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.navigation_home->{
+                    callFragment(0, fragmentHome)
+                }
+                R.id.navigation_keranjang->{
+                    callFragment(1, fragmentKeranjang)
+                }
+                R.id.navigation_akun->{
+                    callFragment(2, fragmentAkun)
+                }
+            }
+
+            false
+        }
+    }
+
+    fun callFragment(int: Int,fragment: Fragment){
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
     }
 }
