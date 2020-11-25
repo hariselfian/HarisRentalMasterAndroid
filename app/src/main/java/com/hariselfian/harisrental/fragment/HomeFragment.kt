b1d3542fe92e7d064a648ed47f7f73ff4e66dacd
hariselfian <harisdj1996@gmail.com>
2020-11-26 00:52:39 +0700

    Recycleview Display Product

--- app/src/main/java/com/hariselfian/harisrental/fragment/HomeFragment.kt
+++ app/src/main/java/com/hariselfian/harisrental/fragment/HomeFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.hariselfian.harisrental.R
+import com.hariselfian.harisrental.adapter.AdapterProduk
 import com.hariselfian.harisrental.adapter.AdapterSlider
+import com.hariselfian.harisrental.model.Produk
 
 /**
  * Kode dibuat dengan cinta oleh Haris Elfian.
@@ -15,15 +19,16 @@ import com.hariselfian.harisrental.adapter.AdapterSlider
 class HomeFragment : Fragment() {
 
     lateinit var vpSlider: ViewPager
+    lateinit var rvProduk: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
         vpSlider = view.findViewById(R.id.vp_slider)
+        rvProduk = view.findViewById(R.id.rv_produk)
 
         val arrSlider = ArrayList<Int>()
         arrSlider.add(R.drawable.slider1)
@@ -33,7 +38,36 @@ class HomeFragment : Fragment() {
         val adapterSlider = AdapterSlider(arrSlider, activity)
         vpSlider.adapter = adapterSlider
 
-        return view
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
+        rvProduk.adapter = AdapterProduk(arrayProduk)
+        rvProduk.layoutManager = layoutManager
+
+        return view
+    }
+
+    val arrayProduk: ArrayList<Produk>get(){
+        val arr = ArrayList<Produk>()
+        val p1 = Produk()
+        p1.nama = "Kebaya Buat Wisuda"
+        p1.harga = "Rp.70.000/Jam"
+        p1.gambar = R.drawable.rental_kebaya
+
+        val p2 = Produk()
+        p2.nama = "Rental Play Station 4"
+        p2.harga = "Rp.5.000/Jam"
+        p2.gambar = R.drawable.rental_ps
+
+        val p3 = Produk()
+        p3.nama = "Kamera Buat Hunting"
+        p3.harga = "Rp.50.000/Jam"
+        p3.gambar = R.drawable.rental_kamera
+
+        arr.add(p1)
+        arr.add(p2)
+        arr.add(p3)
+
+        return arr
     }
 }
