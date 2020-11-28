f80afaa6630919a4fbdb9d50b8b1431a509cb75e
hariselfian <harisdj1996@gmail.com>
2020-11-29 01:51:44 +0700

    Tambah Activity Login UI

---
+++ app/src/main/java/com/hariselfian/harisrental/activity/BaruActivity.kt
@@ -0,0 +1,12 @@
+package com.hariselfian.harisrental.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hariselfian.harisrental.R
+
+class BaruActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baru)
+    }
+}
