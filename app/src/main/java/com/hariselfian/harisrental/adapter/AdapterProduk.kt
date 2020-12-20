5d84bb0f541f1173562700324b540113647f6210
hariselfian <harisdj1996@gmail.com>
2020-12-21 01:03:23 +0700

    - Get Data Product from Api - Display Product

--- app/src/main/java/com/hariselfian/harisrental/adapter/AdapterProduk.kt
+++ app/src/main/java/com/hariselfian/harisrental/adapter/AdapterProduk.kt
@@ -13,16 +13,17 @@ import java.text.NumberFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class AdapterProduk(var data:ArrayList<Produk>):RecyclerView.Adapter<AdapterProduk.Holder>() {
+class AdapterProduk(var data: ArrayList<Produk>) : RecyclerView.Adapter<AdapterProduk.Holder>() {
 
-    class Holder(view: View):RecyclerView.ViewHolder(view){
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val tvNama = view.findViewById<TextView>(R.id.tv_nama)
         val tvHarga = view.findViewById<TextView>(R.id.tv_harga)
         val imgProduk = view.findViewById<ImageView>(R.id.img_produk)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_produk, parent, false)
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_produk, parent, false)
         return Holder(view)
     }
 
@@ -32,9 +33,11 @@ class AdapterProduk(var data:ArrayList<Produk>):RecyclerView.Adapter<AdapterProd
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.tvNama.text = data[position].name
-        holder.tvHarga.text = NumberFormat.getCurrencyInstance(Locale("in", "ID")).format(Integer.valueOf(data[position].harga))
+        holder.tvHarga.text = NumberFormat.getCurrencyInstance(Locale("in", "ID"))
+            .format(Integer.valueOf(data[position].harga))
 //      holder.imgProduk.setImageResource(data[position].image)
-        val image = "http://192.168.43.121/haris_rental/public/storage/produk/" + data[position].image
+        val image =
+            "http://192.168.43.121/haris_rental/public/storage/produk/" + data[position].image
         Picasso.get()
             .load(image)
             .placeholder(R.drawable.product)
