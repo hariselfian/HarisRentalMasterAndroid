1a576a08896b3b7ca9e3bfeaba14c58feb0fb17f
hariselfian <harisdj1996@gmail.com>
2021-01-03 01:54:57 +0700

    Display List Keranjang

--- app/src/main/java/com/hariselfian/harisrental/fragment/KeranjangFragment.kt
+++ app/src/main/java/com/hariselfian/harisrental/fragment/KeranjangFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.hariselfian.harisrental.R
+import com.hariselfian.harisrental.adapter.AdapterProduk
+import com.hariselfian.harisrental.room.MyDatabase
 
 /**
  * Kode dibuat dengan cinta oleh Haris Elfian.
@@ -13,11 +19,59 @@ import com.hariselfian.harisrental.R
 class KeranjangFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_keranjang, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_keranjang, container, false)
+        init(view)
 
+        mainButton()
+        return view
+    }
+
+    private fun displayProduk() {
+        val myDb = MyDatabase.getInstance(requireActivity())
+        val listProduk = myDb!!.daoKeranjang().getAll() as ArrayList
+
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        rvProduk.adapter = AdapterProduk(requireActivity(), listProduk)
+        rvProduk.layoutManager = layoutManager
+    }
+
+    private fun mainButton() {
+        btnDelete.setOnClickListener {
+
+        }
+
+        btnBayar.setOnClickListener {
+
+        }
+    }
+
+    lateinit var btnDelete: ImageView
+    lateinit var rvProduk: RecyclerView
+    lateinit var tvTotal: TextView
+    lateinit var btnBayar: TextView
+    private fun init(view: View) {
+        btnDelete = view.findViewById(R.id.btn_delete)
+        rvProduk = view.findViewById(R.id.rv_produk)
+        tvTotal = view.findViewById(R.id.tv_total)
+        btnBayar = view.findViewById(R.id.btn_sewa)
+    }
+
+    override fun onResume() {
+        displayProduk()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
