b82fc84b670e482bf247f514722d4ea052564b0d
hariselfian <harisdj1996@gmail.com>
2021-01-05 00:37:36 +0700

    Management Keranjang Adapter

---
+++ app/src/main/java/com/hariselfian/harisrental/adapter/AdapterKeranjang.kt
@@ -0,0 +1,75 @@
+package com.hariselfian.harisrental.adapter
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.hariselfian.harisrental.MainActivity
+import com.hariselfian.harisrental.R
+import com.hariselfian.harisrental.activity.DetailProdukActivity
+import com.hariselfian.harisrental.activity.LoginActivity
+import com.hariselfian.harisrental.helper.Helper
+import com.hariselfian.harisrental.model.Produk
+import com.squareup.picasso.Picasso
+import java.text.NumberFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class AdapterKeranjang(var activity: Activity, var data: ArrayList<Produk>) :
+    RecyclerView.Adapter<AdapterKeranjang.Holder>() {
+
+    class Holder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvNama = view.findViewById<TextView>(R.id.tv_nama)
+        val tvHarga = view.findViewById<TextView>(R.id.tv_harga)
+        val imgProduk = view.findViewById<ImageView>(R.id.img_produk)
+        val layout = view.findViewById<CardView>(R.id.layout)
+
+        val btnTambah = view.findViewById<ImageView>(R.id.btn_tambah)
+        val btnKurang = view.findViewById<ImageView>(R.id.btn_kurang)
+        val btnDelete = view.findViewById<ImageView>(R.id.btn_delete)
+
+        val checkBox = view.findViewById<CheckBox>(R.id.checkBox)
+        val tvJumlah = view.findViewById<TextView>(R.id.tv_jumlah)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_keranjang, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.tvNama.text = data[position].name
+        holder.tvHarga.text = Helper().gantiRupiah(data[position].harga)
+
+        val jumlah = data[position].jumlah
+        holder.tvJumlah.text = jumlah.toString()
+//      holder.imgProduk.setImageResource(data[position].image)
+        val image =
+            "http://192.168.43.44/haris_rental/public/storage/produk/" + data[position].image
+        Picasso.get()
+            .load(image)
+            .placeholder(R.drawable.product)
+            .error(R.drawable.product)
+            .into(holder.imgProduk)
+
+        holder.layout.setOnClickListener {
+            val activiti = Intent(activity, DetailProdukActivity::class.java)
+            val str = Gson().toJson(data[position], Produk::class.java)
+            activiti.putExtra("extra", str)
+            activity.startActivity(activiti)
+        }
+    }
+
+}
